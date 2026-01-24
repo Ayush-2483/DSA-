0985147849ef40925e396203427ad82e2cc2e08a
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-01-24 12:01:12 +0530

    Time: 253 ms (12.47%) | Memory: 26 MB (87.53%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    TreeNode* rootNode;
+    bool traversal(TreeNode* root , int target){
+        if(root==NULL) return false;
+        if(root->val==target)
+        return true;
+        if(traversal(root->left,target) || traversal(root->right,target))
+        return true;
+          
+        return false;
+
+    }
+    FindElements(TreeNode* root) {
+        queue<TreeNode*>q;
+        root->val=0;
+        q.push(root);
+        if(!root) return;
+        rootNode=root;
+            while(!q.empty()){
+                TreeNode* node=q.front();
+                q.pop();
+                if(node->left!=NULL){
+                    node->left->val=2*node->val+1;
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    node->right->val=2*node->val+2;
+                    q.push(node->right);
+                }
+            }
+    }
+    
+    bool find(int target) {
+        return traversal(rootNode,target);
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
