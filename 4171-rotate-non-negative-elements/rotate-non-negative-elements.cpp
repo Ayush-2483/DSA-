4400301abc667a2f9dd959e44d1df4ae8a3fba9f
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-01-25 11:53:45 +0530

    Time: 44 ms (12.47%) | Memory: 306.8 MB (57.97%) - LeetSync

---
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rotateElements(vector<int>& nums, int k) {
+        vector<int>positive;
+        for(int x : nums){
+            if(x>=0)
+            positive.push_back(x);
+        }
+        int m=positive.size();
+        if(m==0)
+        return nums;
+        k%=m;
+        rotate(positive.begin(),positive.begin()+k,positive.end());
+        int idx = 0;
+        for(int i = 0;i <nums.size();i++){
+            if(nums[i] >= 0) {
+                nums[i]=positive[idx++];
+            }
+        }
+        
+        return nums;
+
+    }
+};
