a9a59aa820553a62e033d4df0ecc115326554a2e
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-01-26 14:32:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (89.21%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int t[1001];
-    int solve(vector<int>& cost , int i){
-        int n = cost.size();
-        if(i>=cost.size()) return 0;
+    // int t[1001];
+    // int solve(vector<int>& cost , int i){
+    //     if(i>=cost.size()) return 0;
 
-        if(t[i]!=-1)
-        return t[i];
+    //     if(t[i]!=-1)  //Memoization
+    //     return t[i];
 
-        int a = cost[i]+solve(cost,i+1);
-        int b = cost[i]+solve(cost,i+2);
+    //     int a = cost[i]+solve(cost,i+1);
+    //     int b = cost[i]+solve(cost,i+2);
 
-        return t[i]=min(a,b);
-    }
+    //     return t[i]=min(a,b);
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(t,-1,sizeof(t));
-        return min(solve(cost,0),solve(cost,1));
+    //     memset(t,-1,sizeof(t));
+    //     return min(solve(cost,0),solve(cost,1));
+
+    // Bottom - UP 
+    int n = cost.size();
+    if(n==2) 
+    return min(cost[0],cost[1]);
+    for(int i=2;i<n;i++){
+        cost[i]=cost[i]+min(cost[i-1],cost[i-2]);
+    }
+    return min(cost[n-1],cost[n-2]);
     }
 };
