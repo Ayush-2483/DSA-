bbef4233a8148a5767c637c40d78ddbcb70ae614
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-01-28 19:18:55 +0530

    Time: 23 ms (97.33%) | Memory: 13.2 MB (83.59%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
+// reverse approach already done 
     int dp[1001][1001];
-    int solve(string &s1 , string &s2 , int i ,int j){
-        if(i<0 || j<0){
-            return 0;
-        }
+    int solve(string &s,int i,int j){
+        if(i>j) return 0;
+        if(i==j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s1[i]==s2[j]){
-            dp[i][j]= 1+solve(s1,s2,i-1,j-1);
+        if(s[i]==s[j]){
+            dp[i][j]= 2+solve(s,i+1,j-1);
         }
         else{
-            dp[i][j]= max(solve(s1,s2,i-1,j),solve(s1,s2,i,j-1));
+            dp[i][j]= max(solve(s,i+1,j),solve(s,i,j-1));
         }
         return dp[i][j];
     }
-    int longestPalindromeSubseq(string s1) {
+ 
+    
+    int longestPalindromeSubseq(string s) {
         memset(dp,-1,sizeof(dp));
-        string s2 = s1;
-        reverse(s2.begin(),s2.end());
-        return solve(s1,s2,s1.size()-1,s2.size()-1);
+        int i=0,j=s.size()-1;
+        return solve(s,i,j);
+        
         
     }
 };
