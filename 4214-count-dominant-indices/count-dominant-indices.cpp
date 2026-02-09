91ab0c3c71db3065f7ba2b024f1275b3cdd87920
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-02-09 22:34:15 +0530

    Time: 0 ms (100.00%) | Memory: 32.1 MB (0.00%) - LeetSync

--- 4214-count-dominant-indices/count-dominant-indices.cpp
+++ 4214-count-dominant-indices/count-dominant-indices.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int dominantIndices(vector<int>& nums) {
-        int sum=0,average=0,count=0;
-        int n = nums.size();
-        for(int i=0;i<n-1;i++){
-            sum=accumulate(nums.begin()+i+1,nums.end(),0);
-            average=sum/(n-i-1);
-            if(nums[i]>average){
-                count++;
+        // int sum=0,average=0,count=0;
+        // int n = nums.size();
+        // for(int i=0;i<n-1;i++){
+        //     sum=accumulate(nums.begin()+i+1,nums.end(),0);
+        //     average=sum/(n-i-1);
+        //     if(nums[i]>average){
+        //         count++;
+        //     }
+        // }
+        // return count;
+        int n=nums.size();
+        int ans=0,sum=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]>sum/(n-i-1)){
+                ans++;
             }
+            sum+=nums[i];
         }
-        return count;
+        return ans;
 
     }
 };
