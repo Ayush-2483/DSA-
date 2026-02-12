11d82f630927184ff6585993cafcc7811d3eb623
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-02-12 10:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.89%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,26 +1,49 @@
 class Solution {
 public:
-    bool checkBipartiteDFS(vector<vector<int>>& graph,int curr , vector<int>&color , int currColor){
-        color[curr]=currColor;
-        for(int &v : graph[curr]){
-            if(color[v]==color[curr]) return false;
-            if(color[v]== -1){
-                int colorOfV=1-currColor;
-                if(checkBipartiteDFS(graph,v,color,colorOfV)==false)
+    // bool checkBipartiteDFS(vector<vector<int>>& graph,int curr , vector<int>&color , int currColor){
+    //     color[curr]=currColor;
+    //     for(int &v : graph[curr]){
+    //         if(color[v]==color[curr]) return false;
+    //         if(color[v]== -1){
+    //             int colorOfV=1-currColor;
+    //             if(checkBipartiteDFS(graph,v,color,colorOfV)==false)
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+//----------------------------------------------------------------------------------------------
+ bool checkBipartiteBFS(vector<vector<int>>& graph,int curr , vector<int>&color,int currColor){
+      queue<int>q;
+      q.push(curr);
+      color[curr]=currColor;
+      while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        for(int &v : graph[u]){
+            if(color[u]==color[v])  return false;
+            else if(color[v]== -1){
+                color[v]=1-color[u];
+
+                if(checkBipartiteBFS(graph,v,color,color[v])==false)
                 return false;
             }
         }
-        return true;
-    }
+      }
+      return true;
+
+ }
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
         vector<int>color(V,-1);
         for(int i=0;i<V;i++){
             if(color[i]== -1){
-                if(checkBipartiteDFS(graph,i,color,1)==false)
+                if(checkBipartiteBFS(graph,i,color,1)==false)
                 return false;
             }
         }
         return true;
+//------------------------------------------------------------------------------------------------
+
     }
 };
