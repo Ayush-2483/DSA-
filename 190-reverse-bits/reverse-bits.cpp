2f36248f3daefae7ef5cea0ce2d4332155527005
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-02-16 23:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (6.51%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
     int reverseBits(int n) {
-    int result=0;
-     for(int i =0;i<32;i++){
-        result = (result << 1) |(n & 1);
-        n = n >>1;
-    }
-    return result;
+    // int result=0;
+    //  for(int i =0;i<32;i++){
+    //     result = (result << 1) |(n & 1);
+    //     n = n >>1;
+    // }
+    // return result;
+    string binary = "";
+        
+        while(n > 0){
+            binary = char((n % 2) + '0') + binary;
+            n = n / 2;
+        }
+        
+        while(binary.length() < 32){
+            binary = '0' + binary;
+        }
+        
+        reverse(binary.begin(), binary.end());
+        
+        long long  result = 0;
+        long long base = 1;
+        
+        for(int i = 31; i >= 0; i--){
+            if(binary[i] == '1'){
+                result += base;
+            }
+            base *= 2;
+        }
+        
+        return result;
     }
 };
