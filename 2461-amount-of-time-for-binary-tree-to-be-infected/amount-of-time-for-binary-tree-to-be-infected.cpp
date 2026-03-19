a85666f8e407a34eabd8f3c3f4673b018d37a8ea
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-03-19 11:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 128.1 MB (96.09%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,57 +1,73 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void makeGraph(unordered_map<int,vector<int>>&adj,int parent ,TreeNode* curr){
-        if(curr==NULL)  return;
+    int result=INT_MIN;
+    int solve(TreeNode* root,int start){
+        if(root==NULL) return 0;
+        int LH=solve(root->left,start);
+        int RH=solve(root->right,start);
 
-        if(parent!=-1){
-            adj[curr->val].push_back(parent);
+        if(root->val==start){
+            result=max(LH,RH);
+            return -1;
         }
-        if(curr->left){
-            adj[curr->val].push_back(curr->left->val);
+        else if(LH>=0 && RH>=0){
+            return max(LH,RH)+1;
+        } 
+        else{
+            int d= abs(LH)+abs(RH);
+            result=max(result,d);
+
+            return min(LH,RH)-1;
+
         }
-        if(curr->right){
-            adj[curr->val].push_back(curr->right->val);
-        }
-        makeGraph(adj , curr->val,curr->left);
-        makeGraph(adj , curr->val,curr->right);
+        return 0;
     }
+    // void makeGraph(unordered_map<int,vector<int>>&adj,int parent ,TreeNode* curr){
+    //     if(curr==NULL)  return;
+
+    //     if(parent!=-1){
+    //         adj[curr->val].push_back(parent);
+    //     }
+    //     if(curr->left){
+    //         adj[curr->val].push_back(curr->left->val);
+    //     }
+    //     if(curr->right){
+    //         adj[curr->val].push_back(curr->right->val);
+    //     }
+    //     makeGraph(adj ,curr->val,curr->left);
+    //     makeGraph(adj ,curr->val,curr->right);
+    // }
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int,vector<int>> adj;
-        makeGraph(adj , -1 , root);   //O(n)
+        // unordered_map<int,vector<int>> adj;
+        // makeGraph(adj , -1 , root);   //O(n)
 
-        //Apply BFS from start
-        queue<int>que;
-        unordered_set<int>visited;
-        que.push(start);
-        visited.insert(start);
+        // //Apply BFS from start
+        // queue<int>que;
+        // unordered_set<int>visited;
+        // que.push(start);
+        // visited.insert(start);
 
-        int minutes=0;
-        while(!que.empty()){  //O(n)
-            int n = que.size();  //current level size
-            while(n--){
-                int curr=que.front();
-                que.pop();
+        // int minutes=0;
+        // while(!que.empty()){  //O(n)
+        //     int n = que.size();  //current level size
+        //     while(n--){
+        //         int curr=que.front();
+        //         que.pop();
 
-                for(int &neigh : adj[curr]){
-                    if(visited.find(neigh)==visited.end()){
-                        que.push(neigh);
-                        visited.insert(neigh);
-                    }
-                }
-            }
-            minutes++;
-        }
-        return minutes-1;
+        //         for(int &neigh : adj[curr]){
+        //             if(visited.find(neigh)==visited.end()){   //not present in the visited 
+        //                 que.push(neigh);
+        //                 visited.insert(neigh);
+        //             }
+        //         }
+        //     }
+        //     minutes++;
+        // }
+        // return minutes-1;
+
+//---------------------------------------------------------------------------------Approach 2 : 
+      solve(root,start);
+      return result;
+      
     }
 };
