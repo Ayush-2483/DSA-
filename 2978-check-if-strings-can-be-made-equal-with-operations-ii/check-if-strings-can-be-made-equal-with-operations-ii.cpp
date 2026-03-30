3f1a9c67ecc3ce32eedf3b31a55cb2e3b48406df
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-03-30 23:15:46 +0530

    Time: 3 ms (92.53%) | Memory: 19.8 MB (68.97%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        vector<int>even(26,0),odd(26,0);
-        for(int i=0;i<s1.size();i++){
-            if(i % 2 ==0){
-                even[s1[i]-'a']++;
-            }
-            else{
-                odd[s1[i]-'a']++;
-            }
-        }
-        for(int i=0;i<s2.size();i++){
-            if(i % 2 ==0){
-                even[s2[i]-'a']--;
-            }
-            else{
-                odd[s2[i]-'a']--;
+        int n = s1.size();
+        
+        vector<int> even1(26, 0), odd1(26, 0);
+        vector<int> even2(26, 0), odd2(26, 0);
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                even1[s1[i] - 'a']++;
+                even2[s2[i] - 'a']++;
+            } else {
+                odd1[s1[i] - 'a']++;
+                odd2[s2[i] - 'a']++;
             }
         }
-        for(int i=0;i<26;i++){
-            if(even[i]!=0 || odd[i]!=0)
-            return false;
-        }
-        return true;
+        
+        return (even1 == even2) && (odd1 == odd2);
     }
 };
