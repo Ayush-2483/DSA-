8c3273704537d95ce14a1405d9d6c3a74e8ac913
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-05-26 15:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (49.03%) - LeetSync

---
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+       int maxVal=-1,maxIndex=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>maxVal){
+                maxVal=nums[i];
+                maxIndex=i;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(i!=maxIndex && maxVal< 2*nums[i])
+            return -1;
+        }
+        return maxIndex; 
+    }
+};
