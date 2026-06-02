79963a0a697116eea02a1b5c6705282a0915c8d4
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-06-02 12:11:22 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (49.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -5,20 +5,42 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
 
-        vector<vector<int>>temp=matrix;//copy
+        // vector<vector<int>>temp=matrix;//copy
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             for(int k=0;k<n;k++){
+        //                 temp[i][k]=0; //marking ith row as 0
+        //             }
+
+        //             for(int k=0;k<m;k++){
+        //                 temp[k][j]=0; //marking jth col as 0
+        //             }
+        //         }
+        //     }
+        // }
+        // matrix=temp;
+        //-----------------------------------------------------------------
+        //-->Better Approach 
+        vector<bool>row(m,false);
+        vector<bool>col(n,false);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        temp[i][k]=0; //marking ith row as 0
-                    }
-
-                    for(int k=0;k<m;k++){
-                        temp[k][j]=0; //marking jth col as 0
-                    }
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
-        matrix=temp;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==true || col[j]==true){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+
+
     }
 };
