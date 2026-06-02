e88ee6bd9499775233681e4156cb1b7a2de78afc
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-06-02 14:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (66.69%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //Ayush Chaurasiya 
         int n = nums.size();
-        int i=n-2;
-        while(i>=0 && nums[i]>=nums[i+1]){ // first decreasing element nikal rahe hai 
-            i--;
-        }
-        // Wo element find krna hai jo just greater than nums[i] ho
-        if(i>=0){
-            int j=n-1;
-            while(nums[j]<=nums[i]){
-                j--;
+        //first find the gola_index
+        int gola_index= -1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                gola_index=i-1;
+                break;
             }
-            swap(nums[i],nums[j]);
         }
-        reverse(nums.begin()+i+1,nums.end());
+        //find element just greater than gola_index element in right side
+        if(gola_index!=-1){
+        int swap_index=gola_index;
+        for(int j=n-1;j>=gola_index+1;j--){
+            if(nums[j]>nums[gola_index]){
+                swap_index=j;
+                break;
+            }
+        }
+        swap(nums[gola_index],nums[swap_index]);
+        }
+        reverse(nums.begin()+gola_index+1,nums.end());
+
     }
 };
