717c499c08f50f3b2be575348c29518257bd3c10
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-06-02 12:06:16 +0530

    Time: 27 ms (13.44%) | Memory: 21.5 MB (12.74%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        //Approach -1 (Brute Force)
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>>temp=matrix;//copy
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    for(int k=0;k<n;k++){
+                        temp[i][k]=0; //marking ith row as 0
+                    }
+
+                    for(int k=0;k<m;k++){
+                        temp[k][j]=0; //marking jth col as 0
+                    }
+                }
+            }
+        }
+        matrix=temp;
+    }
+};
