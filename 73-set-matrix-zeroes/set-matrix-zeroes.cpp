a4ed1c33d2c50dc875be339081f04c0abe9af4b3
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-06-02 13:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (29.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -22,24 +22,69 @@ public:
         // matrix=temp;
         //-----------------------------------------------------------------
         //-->Better Approach 
-        vector<bool>row(m,false);
-        vector<bool>col(n,false);
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        // vector<bool>row(m,false);
+        // vector<bool>col(n,false);
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             row[i]=true;
+        //             col[j]=true;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(row[i]==true || col[j]==true){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+        //-----------------------------------------------------------------------------
+        //-->Optimal Approach 
+        //check first row impacted hai ya nahi 
+        bool firstRowImpacted=false;
+        for(int col=0;col<n;col++){
+            if(matrix[0][col]==0){
+                firstRowImpacted=true;
+                break;
+            }
+        }
+        //check first col impacted hai ya nahi '
+         bool firstColImpacted=false;
+        for(int row=0;row<m;row++){
+            if(matrix[row][0]==0){
+                firstColImpacted=true;
+                break;
+            }
+        }
+        //set markers in the first row/col
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=true;
-                    col[j]=true;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i]==true || col[j]==true){
+         for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
-        
+        if(firstRowImpacted){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstColImpacted){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+            
+
 
 
     }
