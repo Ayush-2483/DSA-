7622890c2c54eb6290394d5e0736503b815d33b4
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-06-08 00:02:45 +0530

    Time: 44 ms (29.03%) | Memory: 32.3 MB (40.22%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        int n =nums.size();
+        if(n%k!=0) return false;
+        map<int,int>freq;
+        for(int x : nums){
+            freq[x]++;
+        }
+        for(auto &it : freq){
+            if(it.second==0) continue;
+
+            int need=it.second;
+
+            for(int j=0;j<k;j++){
+                if(freq[j+it.first]<need){
+                    return false;
+                }
+                freq[j+it.first]-=need;
+            }
+        }
+        return true;
+    }
+};
