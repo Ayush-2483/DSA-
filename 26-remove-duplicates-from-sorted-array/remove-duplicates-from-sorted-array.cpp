945d456bf19c0d1fcd6c72726f0c5814c1af8056
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-07-14 14:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (53.38%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     int removeDuplicates(vector<int> &nums) {
       int n=nums.size();
-    //   unordered_set<int>seen;
-    //   int index=0;
-    //   for(int num : nums){
-    //     if(seen.find(num)==seen.end()){
-    //         seen.insert(num);
-    //          nums[index]=num;
-    //          index++;
-    //     }
-    //   }
-    //   return index;
-    int i=1;
-    for(int j=1;j<n;j++){
-        if(nums[j]!=nums[j-1]){
-            nums[i]=nums[j];
+      int i=0,j=1;
+      while(j<n){
+        if(nums[i]!=nums[j]){ // hume unique element mil gya 
             i++;
+            nums[i]=nums[j];
         }
-    }
-    return i;
+        j++;
+      }
+      return i+1;
+    
+    
     }
 };
