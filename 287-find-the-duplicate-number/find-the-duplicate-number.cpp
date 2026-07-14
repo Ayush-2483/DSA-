b3b03056e3b8292b0ecb9fe5dfdd27a11467a171
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-07-14 16:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (41.61%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-      int n = nums.size();
-      int low=1,high=n-1,mid,ans,count;
-      while(low<=high){
-        int mid=low+(high-low)/2;
-        count=0;
-        for(auto x : nums) 
-        if(x<=mid)
-        count++;
-        if(count>mid)
-        {
-            ans=mid;
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-      }
-      return ans;
-    // int slow=0,fast=0;
-    // do{
-    //     slow=nums[slow];
-    //     fast=nums[nums[fast]];
-    // }while(slow!=fast);
-    // slow=0;
-    // while(slow!=fast){
-    //     slow=nums[slow];
-    //     fast=nums[fast];
-    // }
+        int slow = nums[0];
+        int fast = nums[0];
 
-    //  return slow;
-// sort(nums.begin(),nums.end());
-// for(int i=1;i<nums.size();i++){
-//     if(nums[i-1]==nums[i])
-//         return nums[i];
-// }
+        slow=nums[slow]; //initially moves chal denge 
+        fast=nums[nums[fast]];
 
-// return -1;
+        while(slow!=fast){
+            slow=nums[slow]; //Detect cycle
+            fast=nums[nums[fast]];
+        }
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];  // find the duplicate number 
+
+            fast=nums[fast];
+        }
+        return slow;
 
     }
 };
