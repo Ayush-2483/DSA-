c476037b078ab9bbc837156fb7d3b71d34fedf60
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-08-02 18:38:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int dp[1001][1001];
+    int LCS(string &text1 , string &text2 , int n , int m){
+        if(n==0 || m==0) return 0;
+        
+        if(dp[n][m]!= -1) return dp[n][m];
+
+        if(text1[n-1]==text2[m-1]){
+            dp[n][m]= 1+LCS(text1,text2,n-1,m-1);
+        }
+        else{
+            dp[n][m]= max(LCS(text1,text2,n,m-1),LCS(text1,text2,n-1,m));
+        }
+        return dp[n][m];
+    }
+    int lcs(string &text1, string &text2) {
+        memset(dp , -1 , sizeof(dp));
+        int n=text1.size(),m=text2.size();
+        return LCS(text1,text2,n,m);
+        
+    }
+};
