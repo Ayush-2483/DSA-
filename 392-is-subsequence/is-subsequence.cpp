11bc53b4fef6d3b989c66f1f454be2a35c86de82
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-08-06 01:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.28%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        int i=0,j=0;
-        while(i<s.size() && j<t.size()){
-            if(s[i]==t[j]){
-                i++;
+     int LCS(string &s1, string &s2,int n,int m){
+        vector<vector<int>>dp(n+1 , vector<int>(m+1,0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1]==s2[j-1]){
+                    dp[i][j]= 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
             }
-            j++;
         }
-        return i==s.size();
+        return dp[n][m];
+        
+    }
+    bool isSubsequence(string s, string t) {
+        // int i=0,j=0;
+        // while(i<s.size() && j<t.size()){
+        //     if(s[i]==t[j]){
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        // return i==s.size();
+        int n=s.size(),m=t.size();
+        int len=LCS(s,t,n,m);
+
+        if(len==s.size()) return true;
+
+        return false;
+
     }
 };
