395014fafeed0db49da084fdbce5654ffcbed7c3
Ayush Chaurasiya <153090346+Ayush-2483@users.noreply.github.com>
2026-08-27 00:00:10 +0530

    Time: 3 ms (38.04%) | Memory: 24.1 MB (82.02%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        for(int i = 1; ; i++) {
+
+            int multiple = k * i;
+
+            bool found = false;
+
+            for (int x : nums) {
+                if (x == multiple) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return multiple;
+            }
+        }
+    }
+};
